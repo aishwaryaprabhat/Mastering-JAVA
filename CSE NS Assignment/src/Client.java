543ff02739054a2ec0dd33f6b7cb4d6837fbfe89
aishwaryaprabhat <aishwaryaprabhat@gmail.com>
2017-04-20 10:36:33 +0800

    Uploading RSA side of Assignment

--- CSE NS Assignment/src/Client.java
+++ CSE NS Assignment/src/Client.java
@@ -1,27 +1,99 @@
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.*;
 import java.net.Socket;
+import java.security.*;
+import java.security.cert.*;
+import java.util.Arrays;
 
 public class Client {
 
-    public final static int SOCKET_PORT = 13267;      // you may change this
-    public final static String SERVER = "localhost";  // localhost
-    public final static String FILE_TO_SEND = "testlocalhost11";  // you may change this
+    public final static int SOCKET_PORT = 13267;
+    public final static String SERVER = "localhost";  // change to desired IP
+    public final static String FILE_TO_SEND = "smallFile.txt";  // change this
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, CertificateException, NoSuchAlgorithmException, NoSuchPaddingException, BadPaddingException, SignatureException, NoSuchProviderException, InvalidKeyException, IllegalBlockSizeException {
 
         Socket sock = null;
         try {
             sock = new Socket(SERVER, SOCKET_PORT);
             System.out.println("Connecting...");
-            //send file
-            sendfile(sock);
+
+            //authenticate and send file
+            authenticate_server(sock);
+
         }finally {
             if (sock != null) sock.close();
         }
 
     }
-    public static void sendfile(Socket sock) throws IOException {
+
+    public static boolean authenticate_server(Socket sock) throws IOException, CertificateException, NoSuchProviderException, NoSuchAlgorithmException, InvalidKeyException, SignatureException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException {
+
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        OutputStream  os = sock.getOutputStream();
+        InputStream is = sock.getInputStream();
+
+        byte[] nonce = new byte[16];
+        byte[] response = new byte[128];
+        int request_for_cert = 1;
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(nonce);
+
+
+        //send nonce
+        os.write(nonce);
+        System.out.println("Nonce sent");
+        //wait to receive encrypted nonce
+        is.read(response);
+        System.out.println("Nonce received");
+        // ask for cert
+        os.write(request_for_cert);
+        System.out.println("Requesting Cert");
+
+        //get CA cert
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        InputStream fisServer = new FileInputStream("CA.crt");
+        X509Certificate CAcert = (X509Certificate)cf.generateCertificate(fisServer);
+        X509Certificate serverCert = (X509Certificate) cf.generateCertificate(is); //input stream
+        System.out.println("Cert received");
+        PublicKey CAkey = CAcert.getPublicKey();
+
+        serverCert.checkValidity();
+        //verify ServerCert using CAkey
+        serverCert.verify(CAkey);
+
+        //Extract public key of server cert
+        PublicKey ServerKey = serverCert.getPublicKey();
+
+        Cipher rsaCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        rsaCipher.init(Cipher.DECRYPT_MODE, ServerKey);
+        byte[] decryptedNonce = rsaCipher.doFinal(response);
+
+
+        //check if nonce matches the decryptedNonce
+        if (Arrays.equals(nonce, decryptedNonce)){
+            System.out.println("Nonce matched");
+            sendfile(sock, ServerKey);
+            return true;
+        }
+        else{
+            System.out.println("Nonce did not match");
+            fos.close();
+            bos.close();
+            is.close();
+            os.close();
+            return false;
+        }
+
+    }
+
+
+    public static void sendfile(Socket sock, PublicKey ServerKey) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
         FileInputStream fis = null;
         BufferedInputStream bis = null;
         OutputStream os = null;
@@ -30,6 +102,14 @@ public class Client {
             // send file
             File myFile = new File (FILE_TO_SEND);
             byte [] mybytearray  = new byte [(int)myFile.length()];
+
+            //encrypt before sending
+            Cipher rsaCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            rsaCipher.init(Cipher.DECRYPT_MODE, ServerKey);
+            rsaCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            rsaCipher.init(Cipher.ENCRYPT_MODE, ServerKey);
+
+
             fis = new FileInputStream(myFile);
             bis = new BufferedInputStream(fis);
             bis.read(mybytearray,0,mybytearray.length);
--- CSE NS Assignment/src/VerifyCert.java
+++ CSE NS Assignment/src/VerifyCert.java
@@ -1,6 +1,7 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.security.*;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
@@ -10,15 +11,20 @@ import java.security.cert.X509Certificate;
  */
 public class VerifyCert {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws FileNotFoundException, CertificateException {
+        verify();
     }
 
-    void verify() throws FileNotFoundException, CertificateException {
+    public static void verify() throws FileNotFoundException, CertificateException, NoSuchProviderException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
 
         InputStream fis = new FileInputStream("CA.crt");
         CertificateFactory cf = CertificateFactory.getInstance("X.509");
         X509Certificate CAcert = (X509Certificate)cf.generateCertificate(fis);
+        PublicKey pbk = CAcert.getPublicKey();
+        System.out.println(pbk);
+
+        //certificate is valid if date and time within validity
+        CAcert.verify(pbk);
 
     }
 }
